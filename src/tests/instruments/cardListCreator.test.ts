ab861a86a9b048392bac737b899f666a38de991c
AlexAbS <abetovalex93@gmail.com>
2024-05-06 14:53:37 +0300

    Выполнен тест для создателя списка карт

---
+++ src/tests/instruments/cardListCreator.test.ts
@@ -0,0 +1,14 @@
+import { Card } from "../../Entities/Card/model"
+import { cardListCreator } from "../../Instruments/Creators/CardList.creator"
+
+describe("cardListCreator creats a list of cards correctly", () => {
+    const list = cardListCreator()
+    test("Every item of list is a Icard object", () => {
+        list.forEach(card => {
+            expect(card instanceof Card).toBe(true)
+        })
+    })
+    test("Length of list is 103", () => {
+        expect(list.length).toBe(103)
+    })
+})
