3a87f23681700b42a30e7e6580dfbbe9fb552900
AlexAbS <abetovalex93@gmail.com>
2024-08-17 00:59:23 +0300

    Сообщения берутся из общей библиотеки. Доработан интерфейс зарегистрированного пользователя

---
+++ src/Adds/Reports/dbReport.type.ts
@@ -0,0 +1,12 @@
+import { reportMessagesLibraryType } from "./reportMessages"
+
+export type DataBaseReportType<datatype = null> = {
+    success: boolean,
+    message: reportMessagesLibraryType,
+    type: DBreportTypeEnum,
+    data?: datatype
+}
+
+export enum DBreportTypeEnum {
+    "Not Found" = "Not Found"
+}
--- src/Entities/RegistratedUser/interface.ts
+++ src/Entities/RegistratedUser/interface.ts
@@ -1,3 +1,6 @@
+import { DataBaseReportType } from "../../Adds/Reports/dbReport.type";
+import { procedureReportType } from "../../Adds/Reports/procedureReport.type";
+import { IDBModel } from "../../Database/interface";
 
 export type gameStatistic = {
     matches: number
@@ -11,4 +14,10 @@ export interface IRegUser {
     hash: string;
     id: string;
     statistic: gameStatistic
+}
+
+export interface IRegUserSelector {
+    database: IDBModel<IRegUser>
+    getRegUser(field: 'hash', value: string):Promise<DataBaseReportType<IRegUser|null>>
+    saveRegUser(user: IRegUser): Promise<DataBaseReportType>
 }
